7f8feb7f3620593ae35b0201a29ebfee3dc9b64b
TyreszB <127250161+TyreszB@users.noreply.github.com>
2024-12-05 22:16:35 -0500

    finished migrations

--- migrations/20241206024223_reviews.ts
+++ migrations/20241206024223_reviews.ts
@@ -5,6 +5,16 @@ export async function up(knex: Knex): Promise<void> {
     table.increments("id").primary();
     table.string("content").notNullable();
     table.integer("rating").notNullable();
+    table
+      .foreign("user_id")
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE");
+    table
+      .foreign("destination_id")
+      .references("id")
+      .inTable("destinations")
+      .onDelete("CASCADE");
   });
 }
 

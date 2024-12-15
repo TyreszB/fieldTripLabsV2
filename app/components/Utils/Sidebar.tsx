71721c87f6a7ca17c7037624fa892bac2d301c3c
TyreszB <127250161+TyreszB@users.noreply.github.com>
2024-12-14 22:38:17 -0500

    working on sidebar

--- app/components/Utils/Sidebar.tsx
+++ app/components/Utils/Sidebar.tsx
@@ -2,20 +2,20 @@
 import React from "react";
 import Image from "next/image";
 import Logo from "../../../public/Logo.png";
-import { CheveronLeft } from "@heroicons/react";
-
-// Sidebar component
+import { UserIcon } from "@heroicons/react/24/outline";
 
 const Sidebar = ({ children }: any) => {
   return (
     <aside className="h-screen">
-      <nav className="h-full flex flex-col bg-gray-50 border border-r shadow-sm">
+      <nav className="h-full flex flex-col bg-gray border-r shadow-sm w-[300px]">
         <div className=" p-4 pb-2 flex justify-between items-center">
-          <Image src={Logo} alt="Logo" width={100} height={100} />
-          <button>
-            <CheveronLeft />
+          <Image src={Logo} alt="Logo" className="w-32" />
+          <button className="p-1.5 rounded-lg bg-white h-5 w-5">
+            <UserIcon className="w-5 h-5" />
           </button>
         </div>
+
+        <ul className="flex-1 px-3">{children}</ul>
       </nav>
     </aside>
   );

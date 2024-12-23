13df8fe5e8469d72afb4650eef69cd162a20376b
TyreszB <127250161+TyreszB@users.noreply.github.com>
2024-12-22 21:28:19 -0500

    starting carousel for places

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,17 @@ export default function Home() {
       <div className="flex flex-1 justify-around">
         <div className="p-5 ">Welcome {capitalize(firstName)},</div>
       </div>
+      <div class="w-full inline-flex flex-nowrap">
+        <ul class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+          {/* add google maps places */}
+        </ul>
+        <ul
+          class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+          aria-hidden="true"
+        >
+          {/* add google maps places */}
+        </ul>
+      </div>
     </main>
   );
 }

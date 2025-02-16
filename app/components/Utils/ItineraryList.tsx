78e96d7535d5b752055280c0007b5d7aec5cff91
TyreszB <127250161+TyreszB@users.noreply.github.com>
2025-02-16 13:51:41 -0500

    working on mock itin page

--- app/components/Utils/ItineraryList.tsx
+++ app/components/Utils/ItineraryList.tsx
@@ -1,9 +1,19 @@
+"use client";
 import React from "react";
 
 const ItineraryList = () => {
+  interface Item {
+    name: string;
+  }
+
+  const [list, setList] = React.useState<Item[]>([]);
+
   return (
-    <div className=" flex justify-center">
-      <div>Itinerary List</div>
+    <div className=" flex justify-center shadow-xl rounded-lg w-[500px] h-[700px] m-10">
+      <div>What To Bring....</div>
+      {list.map((item) => (
+        <li key={item.name}> {item.name}</li>
+      ))}
     </div>
   );
 };

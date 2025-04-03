56ad2ff38053ed58e1410977858038509927ee1d
Tyresz Dev <tyreszdev@MacBookPro.attlocal.net>
2025-04-02 22:15:37 -0400

    working on page

---
+++ app/main/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useSession } from "next-auth/react";
+import capitalize from "./Util/capitalize";
+
+import GoogleMap from "./components/Utils/GoogleMap";
+
+interface SessionData {
+  name: string | null;
+  email: string | null;
+  image: string | null;
+}
+
+export default function Home() {
+  // const { data } = useSession();
+
+  // const firstName = data?.user?.name?.split(" ")[0] ?? "Guest";
+
+  return (
+    <main className="w-screen bg-sky-50">
+      <div className="flex justify-around">
+        {/* <div className="py-5 text-[50px]">Welcome {capitalize(firstName)}</div> */}
+      </div>
+      <div className="w-auto">
+        <GoogleMap />
+      </div>
+    </main>
+  );
+}

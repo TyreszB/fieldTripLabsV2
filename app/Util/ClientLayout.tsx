95fcfad893ad09a15baa2fda866a4a6a6d84e11f
TyreszB <tyresz.brash@gmail.com>
2025-04-24 21:09:37 -0400

    removed auth from wrapper, needs to go under

--- app/Util/ClientLayout.tsx
+++ app/Util/ClientLayout.tsx
@@ -5,21 +5,7 @@ import { useAuth } from "react-oidc-context";
 import AuthWrapper from "./AuthWrapper";
 
 export default function ClientLayout({ children }: { children: React.ReactNode}) {
-  const signOutRedirect = () => {
-    auth.signoutRedirect();
-  }
 
-  const auth = useAuth();
-
-  if (auth.isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (auth.error) {
-    return <div>Error: {auth.error.message}</div>;
-  }
-
-  if (auth.isAuthenticated) {
   return (
     <AuthWrapper>
 
@@ -30,13 +16,6 @@ export default function ClientLayout({ children }: { children: React.ReactNode})
         </div>
     </AuthWrapper>
     
-  )    } else {
-    return (
-      
-        <div>
-          <button onClick={() => auth.signinRedirect()}>Sign in</button>
-          <button onClick={() => signOutRedirect()}>Sign out</button>
-        </div>
-      );
-    }
+  )    
+    
 }

a227191126353bd5347a6736f2d3bf59e86f2e25
TyreszB <tyresz.brash@gmail.com>
2025-04-28 22:26:40 -0400

    refactoring to use amplify

--- app/Util/ClientLayout.tsx
+++ app/Util/ClientLayout.tsx
@@ -1,20 +1,19 @@
 "use client"
 import React from "react";
 import Sidebar from "../components/Utils/Sidebar";
-import { useAuth } from "react-oidc-context";
-import AuthWrapper from "./AuthWrapper";
+
 
 export default function ClientLayout({ children }: { children: React.ReactNode}) {
 
   return (
-    <AuthWrapper>
+    
 
 
         <div className="flex">
             <Sidebar />
             {children}
         </div>
-    </AuthWrapper>
+   
     
   )    
     

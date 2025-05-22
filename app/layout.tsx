45d72815642471a509166610449086c7bdf34e56
TyreszB <tyresz.brash@gmail.com>
2025-05-21 21:08:31 -0400

    working with amplify

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default async function RootLayout({
   return (
    
     <html lang="en">
-      <body className={roboto.className}>
+      <body>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,34 @@
 "use client";
 import capitalize from "./Util/capitalize";
-import { Authenticator } from "@aws-amplify/ui-react";
+import { Authenticator, View, Image } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
-
-
+import Logo from "../public/logo.png";
+import { useTheme } from "@aws-amplify/ui-react";
 
 
 export default function Home() {
   Amplify.configure(outputs);
 
+  const components = {
+    Header() {
+      const { tokens } = useTheme();
+  
+      return (
+        <View textAlign="center" padding={tokens.space.large}>
+          <Image
+            alt="Field Trip Labs logo"
+            src={Logo.src}
+          />
+        </View>
+      );
+    },
+}
+      
+
   return (
-    // TODO: Find a way to edit the Authenticator component 
-    <Authenticator>
+    <Authenticator components={components}>
       {({ signOut, user }) => (
         <main className="w-screen bg-sky-50">
           <div>
@@ -25,3 +40,4 @@ export default function Home() {
     </Authenticator>
   );
 }
+
